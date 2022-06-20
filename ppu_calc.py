9b8d215302da17cd212dfb47756f9e4c129e38b4
pblml <philipp.t.blueml@gmail.com>
2022-06-20 22:47:40 +0200

    Typos

--- ppu_calc.py
+++ ppu_calc.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 class PayPerUse():
 
-    def __init__(self, acq_cost=288000, ref_quantity = 300, interest=0.025,  profit_pu=800, term=6, lower_bound=0.5):
+    def __init__(self, acq_cost=288000, ref_quantity = 50, interest=0.025,  profit_pu=100, term=6, lower_bound=0.5):
         self.acq_cost = acq_cost
         self.term = term
         self.monthly_interest = (np.power(1+interest, (1/12))-1)
@@ -22,13 +22,13 @@ class PayPerUse():
             return
         res_df = df[["run", "counter"]]
         res_df["redemption_pu"] = self.redemption_pu
-        res_df["redemption_rate"] = [c*r if c>=self.lower_bound else self.lower_bound*r for c, r in zip(res_df["counter"], res_df["redemtion_pu"])]
-        res_df["remaining_debt"] = [self.acq_cost - sum(res_df["redemtion_rate"][:i]) for i in range(len(res_df))]
+        res_df["redemption_rate"] = [c*r if c>=self.lower_bound else self.lower_bound*r for c, r in zip(res_df["counter"], res_df["redemption_pu"])]
+        res_df["remaining_debt"] = [self.acq_cost - sum(res_df["redemption_rate"][:i]) for i in range(len(res_df))]
         #!! FORMEL FÜR DIE ZINSBERECHNUNG FALSCH
         res_df["interest"] = res_df["remaining_debt"]*self.monthly_interest
-        res_df["monthly_liability"] = res_df["redemtion_rate"]+res_df["interest"]
+        res_df["monthly_liability"] = res_df["redemption_rate"]+res_df["interest"]
         res_df["turnover"] = self.profit_pu*res_df["counter"]
-        res_df["profits"] = res_df["turnover"]-res_df["redemtion_rate"]
+        res_df["profits"] = res_df["turnover"]-res_df["redemption_rate"]
         res_df["profits_fixed_rate"] = res_df["turnover"]-fixed_rate
         self.df = res_df
         return self
